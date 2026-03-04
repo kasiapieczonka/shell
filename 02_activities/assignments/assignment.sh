065ca84aebf44edc9ab4c1af3f3f732b312092af
Kasia Pieczonka <k.pieczonka@mail.utoronto.ca>
2026-03-03 20:38:23 -0500

    this fixed the bugs in q4, code works now

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -39,10 +39,10 @@ ls ./data/raw
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
 mkdir ./data/processed
-cd ./data/processed
-mkdir server_logs
-mkdir user_logs
-mkdir event_logs
+
+mkdir ./data/processed/server_logs
+mkdir ./data/processed/user_logs
+mkdir ./data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp ./data/raw/*server*.log ./data/processed/server_logs/
