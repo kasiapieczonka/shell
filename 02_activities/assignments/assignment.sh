bf0ac3cdee7d3da4a0944e6e9313f1aeb60cf97c
Kasia Pieczonka <k.pieczonka@mail.utoronto.ca>
2026-03-06 14:09:33 -0500

    Changed code in q8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -55,10 +55,9 @@ cp ./data/raw/*event*.log ./data/processed/event_logs/
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+#cd ./newproject
 touch ./data/inventory.txt
-ls ./data/processed/server_logs > ./data/inventory.txt
-ls ./data/processed/user_logs > ./data/inventory.txt
-ls ./data/processed/event_logs > ./data/inventory.txt
+find data/processed -type f > data/inventory.txt
 
 ###########################################
 
